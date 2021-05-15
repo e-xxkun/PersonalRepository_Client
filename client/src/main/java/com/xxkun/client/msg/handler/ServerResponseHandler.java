2a9c8e8cf02a805d82a1cd8bedf0d0af356e770c
e-xxkun <e_xxkun@163.com>
2021-05-15 23:21:55 +0800

    model building

--- client/src/main/java/com/xxkun/client/msg/handler/ServerResponseHandler.java
+++ client/src/main/java/com/xxkun/client/msg/handler/ServerResponseHandler.java
@@ -3,11 +3,21 @@ package com.xxkun.client.msg.handler;
 import com.google.protobuf.Any;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.xxkun.client.msg.bean.BasePacket;
+import com.xxkun.client.msg.bean.ServerResponse;
 import com.xxkun.client.msg.bean.ServerResponse.*;
 
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ServerResponseHandler extends BasePacketResponseHandler.NextHandler {
+
+    private static Map<ServerResponse.BaseServerResponse.ResponseType, ServerResponseHandler.NextHandler> map = new HashMap<>();
+
+    public static NextHandler getNextHandler(ServerResponse.BaseServerResponse.ResponseType type) {
+        return map.get(type);
+    }
+
     @Override
     BasePacket.Packet.Type getType() {
         return BasePacket.Packet.Type.SERVER;
@@ -28,6 +38,15 @@ public class ServerResponseHandler extends BasePacketResponseHandler.NextHandler
         if (baseServerResponse.getResponseType() == BaseServerResponse.ResponseType.UNRECOGNIZED) {
             return false;
         }
+//        return getNextHandler(baseServerResponse.getResponseType()).consume();
         return true;
+    }
+
+    public abstract static class NextHandler {
+        NextHandler() {
+            map.put(getType(), this);
+        }
+        abstract BaseServerResponse.ResponseType getType();
+        public abstract boolean consume(Any body, InetSocketAddress socketAddress);
     }
 }
